e7e250b9b2419bab6636d0275de5f9fb4e8e9556
jyotidhruba094-sudo <jyotidhruba094@gmail.com>
2025-10-25 18:41:40 +0530

    Update prompt for natural number input

--- assignment/natural_number_sum.c
+++ assignment/natural_number_sum.c
@@ -2,7 +2,7 @@
 int main(){
    int n;
    int s;
-   printf("enter a number for sum of all entered natural numbers: ");
+   printf("enter a natural  number for sum of all entered natural numbers: ");
    scanf("%d", &n);
    s= n*(n+1)/2;
    printf("the sum of the natural numbers %d", s);
