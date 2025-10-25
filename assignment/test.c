cdf86bed38ca6edf323d48a0d81d7e77979d1423
jyotidhruba094-sudo <jyotidhruba094@gmail.com>
2025-10-25 19:08:27 +0530

    Fix conditional statements for number checks

--- assignment/test.c
+++ assignment/test.c
@@ -6,10 +6,10 @@ int main(){
    if ( a>0 ){
        printf(" the number is positive ");
    }
-   if ( a<0 ){
+   else if ( a<0 ){
        printf("the number is negative");
    }
-   if ( a=0){
+   else if ( a=0){
        printf("it is zero");
    }
     return 0;
